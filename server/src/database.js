4131a88426a997d6b8753487eaec9fdd5e25a660
santiago aguirre perez <aguirresantiago81@gmail.com>
2020-10-01 14:34:31 -0300

    Agregado mongoose package, y creada la conexion a la base de datos

---
+++ server/src/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const conexion = 'mongodb://localhost/mean-app';
+mongoose
+    .connect(conexion, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((db)=> console.log("Conectado a la base de datos"))
+    .catch((err)=> console.error("Error al conectar con la base de datos"));
