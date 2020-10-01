b3bf7454d249ada3db9f65c998daf10386220ad5
santiago aguirre perez <aguirresantiago81@gmail.com>
2020-10-01 15:39:41 -0300

    Creado el modelo de Tareas que conecta con la base de datos

---
+++ server/src/models/Tareas.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+
+const TareaScheema = new Scheema ({
+    'nombre': { type: String, required: true},
+    'descripcion': { type: String, required: true},
+    'importancia': { type: String, required: true},
+    'completada': { type: Boolean, required: true},
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+module.exports = model("Tarea", TareaScheema);
